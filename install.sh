3ef91ffe2ee35b342493d2c7bdac75905c31c1ef
n2omatt <n2omatt@amazingcow.com>
2018-02-03 05:04:22 -0200

    - Add the settings and keybindings.

--- install.sh
+++ install.sh
@@ -1,9 +1,34 @@
 #!/usr/bin/env bash
 
+## Stop on errors...
+set -e
 
+##------------------------------------------------------------------------------
+## Vars
 NAME="n2omatt-vscode-ex";
-INSTALL_DIR="$HOME/.vscode/extensions/";
 
+REAL_HOME=$(realpath $HOME);
+SETTINGS_DIR="./settings";
+
+EXT_INSTALL_DIR="$REAL_HOME/.vscode/extensions/";
+SETTINGS_INSTALL_DIR="$REAL_HOME/.config/Code/User/";
+
+## We're on Windows, so the settings path is different.
+if [ -d "$REAL_HOME/AppData/Roaming/Code/User" ]; then
+    SETTINGS_INSTALL_DIR="$REAL_HOME/AppData/Roaming/Code/User";
+fi;
+
+##------------------------------------------------------------------------------
+## Create the target directories.
+mkdir -pv $EXT_INSTALL_DIR;
+mkdir -pv $SETTINGS_INSTALL_DIR;
+
+##------------------------------------------------------------------------------
+## Install Settings.
+cp -rv $SETTINGS_DIR/* "$SETTINGS_INSTALL_DIR";
+
+##------------------------------------------------------------------------------
+## Install Extensions.
 cd ../
-rm -rf "$INSTALL_DIR/$NAME"
-cp -r $NAME "$INSTALL_DIR";
+rm -rf "$EXT_INSTALL_DIR/$NAME"
+cp -r $NAME "$EXT_INSTALL_DIR";
