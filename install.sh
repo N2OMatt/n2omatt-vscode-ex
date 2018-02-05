1deae6765c0fb2b245d51434ba505fe9f98867a9
n2omatt <n2omatt@amazingcow.com>
2018-02-05 06:12:37 -0200

    - Make the install script always find the correct home path.

--- install.sh
+++ install.sh
@@ -1,22 +1,52 @@
 #!/usr/bin/env bash
 
+##------------------------------------------------------------------------------
+## Functions
+find_real_user_home()
+{
+    ## Restore it at the end of the function.
+    if [ $UID == 0 ]; then
+        USER=$(printenv SUDO_USER);
+        if [ -z "$USER" ]; then
+            echo "Installing as root user...";
+            export REAL_USER_HOME="$HOME";
+        else
+            echo "Installing with sudo...";
+            export REAL_USER_HOME=$(getent passwd "$USER" | cut -d: -f6);
+        fi;
+    else
+        echo "Installing as normal user...";
+        export REAL_USER_HOME="$HOME";
+    fi;
+
+    REAL_USER_HOME=$(realpath $REAL_USER_HOME);
+}
+find_real_user_home;
+
+
+##----------------------------------------------------------------------------##
+## Vars                                                                       ##
+##----------------------------------------------------------------------------##
+NAME="n2omatt-vscode-ex";
+SETTINGS_DIR="./settings";
+
+EXT_INSTALL_DIR="$REAL_USER_HOME/.vscode/extensions/";
+SETTINGS_INSTALL_DIR="$REAL_USER_HOME/.config/Code/User/";
+
+## We're on Windows, so the settings path is different.
+if [ -d "$REAL_USER_HOME/AppData/Roaming/Code/User" ]; then
+    SETTINGS_INSTALL_DIR="$REAL_USER_HOME/AppData/Roaming/Code/User";
+fi;
+
+
+##------------------------------------------------------------------------------
 ## Stop on errors...
 set -e
 
 ##------------------------------------------------------------------------------
-## Vars
-NAME="n2omatt-vscode-ex";
-
-REAL_HOME=$(realpath $HOME);
-SETTINGS_DIR="./settings";
-
-EXT_INSTALL_DIR="$REAL_HOME/.vscode/extensions/";
-SETTINGS_INSTALL_DIR="$REAL_HOME/.config/Code/User/";
-
-## We're on Windows, so the settings path is different.
-if [ -d "$REAL_HOME/AppData/Roaming/Code/User" ]; then
-    SETTINGS_INSTALL_DIR="$REAL_HOME/AppData/Roaming/Code/User";
-fi;
+## Print info.
+echo "EXT_INSTALL_DIR      : $EXT_INSTALL_DIR";
+echo "SETTINGS_INSTALL_DIR : $SETTINGS_INSTALL_DIR";
 
 ##------------------------------------------------------------------------------
 ## Create the target directories.
